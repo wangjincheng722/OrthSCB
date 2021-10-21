a079452e10f912ee48d217ab7fd5d3d3429b4366
Wang Jincheng <67851166+wangjincheng722@users.noreply.github.com>
2021-10-21 13:53:48 +0800

    Delete Orthogonalization directory

--- Orthogonalization/Tools.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-import math, sys, os, copy, shutil, struct
-
-def DeleteMakeDir(Path):
-    if os.path.exists(Path):
-        shutil.rmtree(Path)
-    os.makedirs(Path)
-    return
